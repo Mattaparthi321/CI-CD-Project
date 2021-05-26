f1b61b16e455bb31154eb1bd2a7dd40eb2d9c61c
Mattaparthi321 <20041065+Mattaparthi321@users.noreply.github.com>
2021-05-26 20:06:42 +0530

    Jenkins installation
    Jenkins installation

---
+++ jenkins_install.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#jenkins Master istalation in ubuntu
+apt update
+apt-get install -y openjdk-8-jdk
+add package
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sou
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+systemctl status jenkins
