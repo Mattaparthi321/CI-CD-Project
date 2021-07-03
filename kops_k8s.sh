99b591283666a76180c0e4a0a69bb0efbb5a2116
Mattaparthi321 <20041065+Mattaparthi321@users.noreply.github.com>
2021-07-03 11:31:19 +0530

    Update kops_k8s.sh

--- kops_k8s.sh
+++ kops_k8s.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 # installtion kubernates cluster KOPS in ubuntu
-wget https://github.com/kubernetes/ko
-ps/releases/download/v1.20.1/kops-linux-amd64
+wget https://github.com/kubernetes/kops/releases/download/v1.20.1/kops-linux-amd64
 chmod 700 kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
