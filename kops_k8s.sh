f62baf3f377dfb2d5f7d2f2ee3a59729f1d0a42b
Mattaparthi321 <20041065+Mattaparthi321@users.noreply.github.com>
2021-07-03 15:51:35 +0530

    Update kops_k8s.sh

--- kops_k8s.sh
+++ kops_k8s.sh
@@ -1,5 +1,15 @@
 #/bin/bash
 # installtion kubernates cluster KOPS in ubuntu
+# prerequset AWS accout
+# s3bucketand Route53 confguration
+# This script install kops and kubectl and AWS cli
+# once this script run then do below steps
+# creat IAM ROLE and atach to management server
+# AWS Access Key ID [****************6J]:
+# AWS Secret Access Key [****************s1]:
+# Default region name [s3://msubhashini.xyz]: us-east-1
+# ssh-keygen then add you private key  content with key gen to idrsa.pub 
+
 wget https://github.com/kubernetes/kops/releases/download/v1.20.1/kops-linux-amd64
 chmod 700 kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
